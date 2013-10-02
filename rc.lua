4caa6e48689c8ee528cc2f5433abdfe9acb3e179
Erhard Muresan <erhard_m07@yahoo.com>
2013-10-02 10:57:41 +0300

    execute xscreensaver with no splash

--- rc.lua
+++ rc.lua
@@ -438,3 +438,4 @@ client.add_signal("focus", function(c) c.border_color = beautiful.border_focus e
 client.add_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
 -- }}}
 os.execute("synclient TapButton1=1")
+os.execute("xscreensaver -no-splash")
